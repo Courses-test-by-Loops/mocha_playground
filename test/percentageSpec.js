4e1f044bf518d007c7b8b6d20454ea0afbae92f7
Ludo <ludoloops@protonmail.com>
2020-05-31 03:17:10 +0200

    feat ajout chai

--- test/percentageSpec.js
+++ test/percentageSpec.js
@@ -1,11 +1,12 @@
-const assert = require('assert');
+const { assert } = require('chai');
+const { expect } = require('chai');
 const Percentage = require('../lib/Percentage');
 
 describe('Percentage', () => {
   describe('#evaluation', () => {
     it('should give an evolution', () => {
-      assert.equal(Percentage.evolution(100, 200), 100);
-      assert.equal(Percentage.evolution(100, 150), 50);
+      expect(Percentage.evolution(100, 200)).be.equal(100);
+      expect(Percentage.evolution(100, 150)).be.equal(50);
       assert.equal(Percentage.evolution(100, 50), -50);
     });
     it('should handle 0 evolution', () => {
